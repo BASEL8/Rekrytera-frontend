18c37234c11020810d3a4e3080d692f27d34eeec
BASEL8 <basel84mn@gmail.com>
2020-04-24 16:34:23 +0200

    first

--- src/app.config.js
+++ src/app.config.js
@@ -2,9 +2,9 @@ module.exports = {
   publicRuntimeConfig: {
     APP_NAME: 'Rekrytera',
     API_DEVELOPMENT: 'http://localhost:8000/api',
-    API_PRODUCTION: 'http://159.203.180.23/api',
-    PRODUCTION: false,
+    API_PRODUCTION: 'https://rekrytera-backend.herokuapp.com/api',
+    PRODUCTION: true,
     DOMAIN_DEVELOPMENT: 'http://lcoalhost:3000',
-    DOMAIN_PRODUCTION: 'http://159.203.180.23',
+    DOMAIN_PRODUCTION: 'https://rekrytera-frontend.herokuapp.com',
   }
 }
