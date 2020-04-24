c2dc5407a8dbaaa97553736cbde7fbb37371f0b5
BASEL8 <basel84mn@gmail.com>
2020-04-24 15:54:58 +0200

    start

---
+++ src/app.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: 'Rekrytera',
+    API_DEVELOPMENT: 'http://localhost:8000/api',
+    API_PRODUCTION: 'http://159.203.180.23/api',
+    PRODUCTION: false,
+    DOMAIN_DEVELOPMENT: 'http://lcoalhost:3000',
+    DOMAIN_PRODUCTION: 'http://159.203.180.23',
+  }
+}
